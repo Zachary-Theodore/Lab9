0dd09d032c072ed5ab8670b7d1e8b2ae4260488a
Zachary_Theodore <fk9836@wayne.edu>
2018-07-30 21:35:34 -0400

    Switching over to objects

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -11,99 +11,60 @@ namespace Lab9
     {
         static void Main(string[] args)
         {
+            List<Students> students = new List<Students>();
+            Students temp1 = new Students();
             List<string> studentNames = new List<string>()
-        {
+            {
             "Mr. Meeseeks",
             "Lord Canti",
             "Solid Snake",
             "Cloud Strife",
             "King Mrgl-Mrgl"
-        };
+            };
             List<string> hobbies = new List<string>()
-        {
+            {
             "Completeing Tasks Before the Pain Drives them insane.",
             "Starting fires.",
             "Redacted",
             "Chocabo Breeding",
             "Observing the life of a Murloc"
-        };
+            };
             List<string> origin = new List<string>()
-        {
+            {
             "Meeseeks Box",
             "Naota Nandaba's Head",
             "Redacted",
             "Nibelheim",
             "Unclear"
-        };
+            };
             List<string> birthdays = new List<string>()
-        {
+            {
             "Now",
             "Then",
             "Later",
             "12/12/1986",
             "Party Time"
-        };
-
-            Console.WriteLine("Would you like to add any info.(Y/N)");
-            string userInput = Console.ReadLine();
-            bool truefalse = true;
-            while (truefalse)
+            };
+            for (int i = 0; i < studentNames.Count; i++)
             {
-                if (Regex.IsMatch(userInput, @"^y|Y$"))
-                {
-                    Console.WriteLine("Enter a Name to be stored:");
-                    studentNames.Add(Console.ReadLine());
-                    Console.WriteLine("Enter a hobby:");
-                    hobbies.Add(Console.ReadLine());
-                    Console.WriteLine("Enter a origin:");
-                    origin.Add(Console.ReadLine());
-                    Console.WriteLine("Enter a birthday:");
-                    birthdays.Add(Console.ReadLine());
-                    truefalse = false;
-                }
-                else if (Regex.IsMatch(userInput, @"^n|N$"))
-                {
-                    Console.WriteLine("Ok Lets Continue");
-                    truefalse = false;
-                }
-                else
-                {
-                    Console.WriteLine("Please enter a y or n");
-                    userInput = Console.ReadLine();
-                }
+                temp1.Name = studentNames[i];
+                temp1.Hobbie = hobbies[i];
+                temp1.Origin = origin[i];
+                temp1.Birthday = birthdays[i];
+
+                students.Add(temp1);
             }
 
-            Console.WriteLine("Enter a Number to learn more about a subject.");
-            int nameNum = int.Parse(Console.ReadLine());
-            while (nameNum<0 || nameNum>studentNames.Count)
+            foreach (Students e in students)
             {
+                Console.WriteLine($"{e.Name}, {e.Hobbie}, {e.Origin}, {e.Birthday}");
+                Console.WriteLine("===================================================");
+            }
 
-                Console.WriteLine("Error: Entered a value either out of Range or wrong format");
-                Console.WriteLine("Please enter a number between 1 and 5;");
-                nameNum = int.Parse(Console.ReadLine());
-            }
-            int indexName = nameNum;
-            Console.WriteLine($"You chose {studentNames[indexName - 1]}!!!");
-            Console.WriteLine("Would you like to learn more?(enter Origin or Hobby)");
-            string pick1 = Console.ReadLine();
-            while (!Regex.IsMatch(pick1, @"^Origin|origin|Hobby|hobby$"))
-            {
 
-                Console.WriteLine("Error: Please make sure to only enter one of the two options");
-                Console.WriteLine("Please enter either Hobby or Origin");
-                pick1 = Console.ReadLine();
-            }
-            if (pick1 == "Hobby" || pick1 == "hobby")
-            {
-                Console.WriteLine($"You chose {pick1}, {studentNames[indexName - 1]} loves to {hobbies[indexName - 1]} ");
-            }
-            else
-            {
-                Console.WriteLine($"You chose {pick1}, {studentNames[indexName - 1]} comes from {origin[indexName - 1]}. ");
-            }
         }
 
 
     }
-    
+
 }
---
+++ Lab9/Students.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Students
+    {
+        private string name;
+        private string hobbie;
+        private string origin;
+        private string birthday;
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public string Hobbie
+        {
+            set { hobbie = value; }
+            get { return hobbie; }
+        }
+        public string Origin
+        {
+            set { origin = value; }
+            get { return origin; }
+        }
+        public string Birthday
+        {
+            set { birthday = value; }
+            get { return birthday; }
+        }
+
+    }
+}
